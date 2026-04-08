f7ddc7f114ab8f4d48c7cd614c2a02bafeed19c3
Spencer Campbell <spence@twentyhertz.com>
2026-04-07 14:13:06 -1000

    chore: tweak table header styling

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -70,7 +70,7 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
     <th
       data-slot="table-head"
       className={cn(
-        "text-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0",
+        "text-muted-foreground h-10 px-2 text-left align-middle text-xs font-semibold tracking-wide whitespace-nowrap uppercase [&:has([role=checkbox])]:pr-0",
         className,
       )}
       {...props}
